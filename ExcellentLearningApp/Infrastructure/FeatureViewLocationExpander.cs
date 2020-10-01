700a024c078065ee62c642531160d8942f91a7e3
Piotr Domitrz <domitrz.piotr2@gmail.com>
2020-10-02 00:17:47 +0200

    The solution and project were created

---
+++ ExcellentLearningApp/Infrastructure/FeatureViewLocationExpander.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExcellentLearningApp.Infrastructure
+{
+    public class FeatureLocationExpander : IViewLocationExpander
+    {
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+        }
+
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context,
+            IEnumerable<string> viewLocations)
+        {
+            // The old locations are /Views/{1}/{0}.cshtml and /Views/Shared/{0}.cshtml where {1} is the controller and {0} is the name of the View
+            // Replace /Views with /Features
+
+            var expandViewLocations = new List<string>
+            {
+                "/Features/{1}/{0}.cshtml",
+                "/Features/Shared/{0}.cshtml",
+            };
+
+            return expandViewLocations;
+        }
+    }
+}
